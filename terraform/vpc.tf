cafe67ca3a634a38ff05c0a638b571336c8ffab2
Bassam Ismail <skippednote@gmail.com>
2023-04-25 10:25:29 +0530

    update message

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -52,3 +52,36 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block = "::/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+resource "aws_route_table_association" "route1" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.sn1.id
+}
+
+resource "aws_route_table_association" "route2" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.sn2.id
+}
+
+resource "aws_route_table_association" "route3" {
+  route_table_id = aws_route_table.rt.id
+  subnet_id = aws_subnet.sn3.id
+}
